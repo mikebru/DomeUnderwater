c3d057959694d8f8c464a3d124c3af018e230fa4
mikebru <michael.bruner3@gmail.com>
2018-12-17 19:51:55 -0600

    Initial commit

---
+++ Assets/Dome capture/DomeSetup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DomeSetup : MonoBehaviour {
+
+    public bool ExteriorMapping = false;
+    public int NumberOfCams = 4;
+
+        
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# DomeUnderwater
