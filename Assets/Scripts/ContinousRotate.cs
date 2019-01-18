80b4deee12ab497b6af0e9404421e71775a0b4e3
fakaiwen <fakaiwen@utexas.edu>
2019-01-18 15:12:09 -0600

    Added i dome dissolve and cargo sequence

--- Assets/Scripts/ContinousRotate.cs
+++ Assets/Scripts/ContinousRotate.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ContinousRotate : MonoBehaviour
 {
+
+    public Vector3 rotateVector;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class ContinousRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(rotateVector);
     }
 }
--- Assets/Scripts/SceneTimer.cs
+++ Assets/Scripts/SceneTimer.cs
@@ -5,21 +5,64 @@ using UnityEngine.Playables;
 
 public class SceneTimer : MonoBehaviour
 {
+    public bool AutoStart = false;
+
     public PlayableDirector cargoTimeline;
+    public TrashGenerator trashGen;
+
+    public float fadeTime = 10;
+    public MeshRenderer DissolveDome;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (AutoStart == true)
+        {
+            StartCoroutine(CargoShipWait());
+        }
     }
 
     public IEnumerator CargoShipWait()
     {
         //wait for 3 minutes 
-        yield return new WaitForSeconds(180);
+        yield return new WaitForSeconds(10);
 
 
         cargoTimeline.Play();
+
+        StartCoroutine(DumpWait());
+    }
+
+    public IEnumerator DumpWait()
+    {
+        //wait for 30 seconds
+        yield return new WaitForSeconds(20);
+
+        trashGen.spawnRate = .1f;
+
+
+        //wait for 2.5 minutes
+        yield return new WaitForSeconds(150);
+
+        StartCoroutine(FadeOut());
+    }
+
+    public IEnumerator FadeOut()
+    {
+
+        float t = 0;
+
+        while(t < fadeTime)
+        {
+            t += Time.deltaTime;
+
+            DissolveDome.material.SetFloat("_Amount", Mathf.Lerp(1, 0, t / fadeTime));
+
+            yield return new WaitForFixedUpdate();
+        }
+
+
+
     }
 
 
